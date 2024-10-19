c081810472f9a4c23f163b558ef51b3f27f77bac
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2024-10-19 21:29:24 +0700

    Time: 1 ms (99.88%), Space: 42.3 MB (34.3%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,18 +1,20 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder res = new StringBuilder();
+
         int lenA = a.length() - 1;
         int lenB = b.length() - 1;
+
         int carry = 0;
 
-        while (lenA >= 0 || lenB >= 0 || carry > 0) {
+        while(lenA >= 0 || lenB >= 0 || carry > 0){
             int sum = carry;
 
-            if (lenA >= 0) {
+            if(lenA >= 0){
                 sum += a.charAt(lenA--) - '0';
             }
 
-            if (lenB >= 0) {
+            if(lenB >= 0){
                 sum += b.charAt(lenB--) - '0';
             }
 
