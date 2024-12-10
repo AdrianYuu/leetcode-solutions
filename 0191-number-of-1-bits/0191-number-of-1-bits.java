475d41b2a4d5bd460f6940ce54daa8a3585cbb21
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2024-12-11 05:40:52 +0700

    Time: 0 ms (100%), Space: 40.8 MB (37.18%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,17 +1,11 @@
 class Solution {
     public int hammingWeight(int n) {
-        StringBuilder s = new StringBuilder();
-
-        while(n != 0){
-            s.append(n % 2);
-            n /= 2;
-        }
-        
-        s.reverse();
 
         int sum = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '1') sum++;
+
+        while(n != 0){
+            if(n % 2 == 1) sum++;
+            n /= 2;
         }
 
         return sum;
