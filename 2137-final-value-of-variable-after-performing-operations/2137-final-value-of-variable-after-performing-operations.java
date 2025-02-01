f638bb9e00c5845e9d93421c2b0e711f3af3c918
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2025-02-01 21:19:58 +0700

    Time: 1 ms (77.6%), Space: 43.2 MB (13.9%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int result = 0;
+
+        for(String s : operations){
+            if(s.contains("-")) result--;
+            else result++;
+        }
+
+        return result;
+    }
+}
