b2ed939dd01ab6a0ebeefb4fdeb6aaa004e65d0f
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2025-03-16 08:10:26 +0700

    Time: 1 ms (99.5%), Space: 46.2 MB (19.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        int len = nums.length;
+
+        for(int i = 0; i < len; i++){
+            if(nums[i] != 0){
+                nums[idx++] = nums[i];
+            }
+        }
+
+        while(idx < len) nums[idx++] = 0;
+    }
+}
