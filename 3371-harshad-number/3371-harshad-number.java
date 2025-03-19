4804e67f3385890b33fc3ccab21ea69e206c635d
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2025-03-19 17:37:05 +0700

    Time: 0 ms (100%), Space: 40.9 MB (8.85%) - LeetHub

---
+++ 3371-harshad-number/3371-harshad-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int num = x;
+        int sum = 0;
+
+        while(num > 0){
+            sum += (num % 10);
+            num /= 10;
+        } 
+
+        if(x % sum != 0) return -1;
+
+        return sum;
+    }
+}
