f3baf4b0d62da7f3c87e5e2bc41b73fb9b96ea3f
Adrian Yu <114144240+AdrianYuu@users.noreply.github.com>
2025-04-05 08:54:48 +0700

    Time: 15 ms (9.87%), Space: 45.4 MB (6.56%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,35 @@
+class Solution {
+    private void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets){
+        if(index == nums.length){
+            subsets.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[index]);
+        generateSubsets(nums, index + 1, subset, subsets);
+
+        subset.remove(subset.size() - 1);
+        generateSubsets(nums, index + 1, subset, subsets);
+    }
+
+    public int subsetXORSum(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<>();
+
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+
+        int result = 0;
+        for(List<Integer> subset : subsets){
+            int total = 0;
+
+            for(int num : subset){
+                total ^= num;
+            }
+
+            result += total;
+        }
+
+        return result;
+    }
+
+    
+}
